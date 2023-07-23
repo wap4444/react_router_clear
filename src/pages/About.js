d02bf92a028c55fd5201d044cfcc0079ae4b2b83
wap4444 <wap4444@mail.ru>
2023-07-23 13:51:43 +0600

    11212

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="container">
+      <h2>About Us</h2>
+      <p>We are a team of developers building awesome things.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h2>Contact Us</h2>
+      <p>Contact us at contact@example.com</p>
+    </div>
+  );
+};
+
+export default Contact;
